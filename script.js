19b34cb0c129d829349ec775ca9c6f159a8236b3
MarriaPetryshyn <petryshyn.main@gmail.com>
2017-05-18 15:37:30 +0300

    added index and js files. Also added currency convertion

---
+++ script.js
@@ -0,0 +1,8 @@
+var euros = prompt('Please, enter a sum to convert euros to hryvni');
+var dollars = prompt('Please, enter a sum to convert dollars to hryvni');
+
+var conversionEuros = euro * 28.87;
+var conversionDollars = dollars * 26,22;
+var conversionEuroToDollar = euro * 1.09;
+
+alert(euros, 'euros are equal', conversionEuros, 'grns', dollars, 'dollars are equal' conversionDollars, euro, 'euro is equal', conversionEuroToDollar, 'dollars.');
