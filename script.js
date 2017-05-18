c371d2d5b9bd8bb4fe2d05ba725e5ab0468e08e7
MarriaPetryshyn <petryshyn.main@gmail.com>
2017-05-18 16:33:58 +0300

    fixed currency converter

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var euros = prompt('Please, enter a sum to convert euros to hryvni');
-var dollars = prompt('Please, enter a sum to convert dollars to hryvni');
+var euro = prompt('Please, enter a sum to convert euros to hryvni');
+var dollar = prompt('Please, enter a sum to convert dollars to hryvni');
 
 var conversionEuros = euro * 28.87;
-var conversionDollars = dollars * 26,22;
+var conversionDollars = dollar * 26.22;
 var conversionEuroToDollar = euro * 1.09;
 
-alert(euros, 'euros are equal', conversionEuros, 'grns', dollars, 'dollars are equal' conversionDollars, euro, 'euro is equal', conversionEuroToDollar, 'dollars.');
+alert(euro + ' euros are equal ' + conversionEuros + ' grns, ' + dollar + ' dollars are equal ' + conversionDollars + ' and ' + euro + ' euro is equal ' + conversionEuroToDollar + ' dollars.');
